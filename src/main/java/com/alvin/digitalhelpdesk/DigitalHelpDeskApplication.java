116050f873c20ceecbd7c56734a2df7e970f9383
st-vin <onsongoalvin@gmail.com>
2026-06-17 16:27:55 +0300

    initial setup and configuration

---
+++ src/main/java/com/alvin/digitalhelpdesk/DigitalHelpDeskApplication.java
@@ -0,0 +1,13 @@
+package com.alvin.digitalhelpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalHelpDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalHelpDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alvin/digitalhelpdesk/DigitalHelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvin.digitalhelpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalHelpDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
